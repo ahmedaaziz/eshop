f49fecf57c9b96bda06737f7fc7c2b186bf2fdfa
ahmed0abdelaziz@gmail.com <ahmed0abdelaziz@gmail.com>
2021-08-23 15:15:04 +0200

    Fix server.js issues

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
+//Install express server
 const express = require("express");
 const path = require("path");
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + "/dist/eshop"));
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/index.html"));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/eshop/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
